72d963f9c39028db33376d912f2a15a541faaf00
Azafo Alexandre Cossa <azafoalexandre@outlook.com>
2021-05-25 17:05:19 +0200

    Added Angular RouterModule

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,17 +1,11 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminLayoutComponent } from './admin-layout.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
-
-
 @NgModule({
-  declarations: [
-    AdminLayoutComponent,
-    DashboardComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [AdminLayoutComponent, DashboardComponent],
+  imports: [CommonModule, RouterModule],
 })
-export class AdminModule { }
+export class AdminModule {}
