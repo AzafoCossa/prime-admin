22e01406deabf71bb0224a5e19aadffb17c7ad04
Azafo Alexandre Cossa <azafoalexandre@outlook.com>
2021-05-25 16:48:07 +0200

    created admin module

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AdminLayoutComponent } from './admin-layout.component';
+
+
+
+@NgModule({
+  declarations: [
+    AdminLayoutComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AdminModule { }
