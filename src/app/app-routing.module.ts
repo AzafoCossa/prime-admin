5bc1d8e37ca65bc3a0170e2a7195bfbe5370d159
Azafo Alexandre Cossa <azafoalexandre@outlook.com>
2021-05-25 17:03:53 +0200

    Basic routing structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { AdminLayoutComponent } from './admin/admin-layout.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'admin',
+    component: AdminLayoutComponent,
+    children: [{ path: '', component: DashboardComponent }],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
