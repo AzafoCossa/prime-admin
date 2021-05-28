ebdac5a732b00a912a64677fbf0a7e46c7e50e44
Azafo Alexandre Cossa <azafoalexandre@outlook.com>
2021-05-28 11:41:37 +0200

    Inicial prime modules

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,11 +1,39 @@
+import { PanelModule } from 'primeng/panel';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminLayoutComponent } from './admin-layout.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { PanelMenuModule } from 'primeng/panelmenu';
+import { BadgeModule } from 'primeng/badge';
+import { MenuModule } from 'primeng/menu';
+import { InputTextModule } from 'primeng/inputtext';
+import { ProfileComponent } from './profile/profile.component';
+import { AvatarModule } from 'primeng/avatar';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AdminLayoutComponent, DashboardComponent],
-  imports: [CommonModule, RouterModule],
+  declarations: [
+    AdminLayoutComponent,
+    DashboardComponent,
+    SideBarComponent,
+    ProfileComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    FormsModule,
+    PanelModule,
+    ButtonModule,
+    CardModule,
+    PanelMenuModule,
+    InputTextModule,
+    MenuModule,
+    AvatarModule,
+    BadgeModule,
+  ],
 })
 export class AdminModule {}
