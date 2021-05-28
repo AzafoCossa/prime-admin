4a70de44c2e8b08d61b914bc92acab440133b1aa
Azafo Alexandre Cossa <azafoalexandre@outlook.com>
2021-05-28 11:40:35 +0200

    Admin layout finished

--- src/app/admin/admin-layout.component.ts
+++ src/app/admin/admin-layout.component.ts
@@ -1,15 +1,40 @@
+import { MenuItem } from 'primeng/api';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-admin-layout',
   templateUrl: './admin-layout.component.html',
-  styleUrls: ['./admin-layout.component.scss']
+  styleUrls: ['./admin-layout.component.scss'],
 })
 export class AdminLayoutComponent implements OnInit {
+  public display = true;
+  public search: string = '';
+  public settingsOptions: MenuItem[] = [];
+  constructor() {}
 
-  constructor() { }
+  public screenSize: any;
 
   ngOnInit(): void {
+    this.windowSize();
+    window.addEventListener('resize', () => {
+      console.log(this.screenSize);
+      this.windowSize();
+    });
+
+    this.settingsOptions = [
+      { label: 'Profile', icon: 'pi pi-fw pi-user' },
+      { label: 'Settings', icon: 'pi pi-fw pi-cog' },
+      { label: 'Help Center', icon: 'pi pi-fw pi-question-circle' },
+      { separator: true },
+      { label: 'Logout', icon: 'pi pi-fw pi-sign-out' },
+    ];
   }
 
+  windowSize() {
+    return (this.screenSize = window.innerWidth);
+  }
+
+  searchTerm() {
+    console.log('Search clicked...');
+  }
 }
